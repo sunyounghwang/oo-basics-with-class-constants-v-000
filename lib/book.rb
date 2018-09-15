e6389cf4aa09e2ab4aad1e15681fa0f1a420dc16
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-15 20:20:23 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -10,11 +10,11 @@ class Book
 
   def genre=(genre)
     @genre = genre
-    GENRES << self.genre
+    GENRES << genre
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
+
 end
